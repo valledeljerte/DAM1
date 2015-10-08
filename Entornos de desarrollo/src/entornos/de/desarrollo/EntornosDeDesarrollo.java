40caffbcb6b32e75802bc1186398daae26fbe718
rrgarciac01 <rrgarciac01@aula1-21>
2015-10-08 17:59:51 +0200

    Hecho un cambio

--- Entornos de desarrollo/src/entornos/de/desarrollo/EntornosDeDesarrollo.java
+++ Entornos de desarrollo/src/entornos/de/desarrollo/EntornosDeDesarrollo.java
@@ -16,6 +16,7 @@ public class EntornosDeDesarrollo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println( " Para probar el entorno de desarrollo "); 
     }
     
 }
