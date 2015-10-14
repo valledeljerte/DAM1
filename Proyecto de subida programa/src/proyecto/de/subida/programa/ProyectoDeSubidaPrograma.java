f7faf3ca22e423e570ca6de68b16e8a2f24e7c96
rrgarciac01 <rrgarciac01@aula1-21>
2015-10-14 19:50:55 +0200

    Hecho un cambio

---
+++ Proyecto de subida programa/src/proyecto/de/subida/programa/ProyectoDeSubidaPrograma.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.de.subida.programa;
+
+/**
+ *
+ * @author rrgarciac01
+ */
+public class ProyectoDeSubidaPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cte2=2;
+        double ctepi=3.1416;
+        double cte3=3.55;
+        double longitud=0;
+        longitud = cte2 * ctepi * cte3;    
+        System.out.println("La longitud de una circunferencia cuyo radio vale 3 sería igual a: " +longitud);  
+    }
+    }
+    
+}
